8a896ec5ddb99ccfefbc805fee10f6d0a513503b
Federico Grandi <fgrandi30@gmail.com>
2019-07-20 21:26:33 +0200

    Fix discordboats Wtf man?!?

--- src/Interface/DiscordBoats.js
+++ src/Interface/DiscordBoats.js
@@ -6,42 +6,28 @@ const ServiceBase = require('./ServiceBase')
  */
 class DiscordBoats extends ServiceBase {
   /**
-   * Gets a list of bots on this service
-   */
-  getBots(){
-    return this._request({ url: `https://api.discordapps.dev/api/v2/bots` });
-  }
-
-  /**
    * Gets the bot listed for this service
    * @param {string} id The bot's ID.
    */
-  getBot(id){
-    return this._request({ url: `https://api.discordapps.dev/api/v2/bots/${id}` });
-  }
-
-  /**
-   * Gets the bot widget for this service
-   * @param {string} id The bot's ID.
-   */
-  getWidget(id){
-    return this._request({ url: `https://discord.boats/api/v2/widget/${id}` });
+  getBot(id) {
+    return this._request({ url: `https://discord.boats/api/v2/bot/${id}` });
   }
 
   /**
    * Gets the user listed for this service
    * @param {string} id The user's ID.
    */
-  getUser(id){
-    return this._request({ url: `https://api.discordapps.dev/api/v2/user/${id}` });
+  getUser(id) {
+    return this._request({ url: `https://discord.boats/api/v2/user/${id}` });
   }
 
   /**
    * Whether or not a user has voted for a bot
    * @param {string} id The bot's ID.
+   * @param {string} userID The user's ID.
    */
-  userVoted(id, userID){
-    return this._request({ url: `https://discord.boats/api/v2/bot/${id}/voted?user=${userID}` });
+  userVoted(id, userID) {
+    return this._request({ url: `https://discord.boats/api/v2/bot/${id}/voted?id=${userID}` });
   }
 }
 
