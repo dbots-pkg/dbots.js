a68f909b528e356f73b852a01fbdb94dd17e34b5
Snazzah <suggesttosnazzy@gmail.com>
2019-12-24 18:08:57 -0600

    Change lint configs

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+      "commonjs": true,
+      "es6": true,
+      "node": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+      "ecmaVersion": 8,
+      "sourceType": "module"
+  },
+  "rules": {
+  }
+};
