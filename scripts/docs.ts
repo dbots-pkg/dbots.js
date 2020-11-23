92b245a5b1c2965610958813f4fd799d30249c41
Federico Grandi <fgrandi30@gmail.com>
2020-11-23 15:47:51 +0100

    docs: fix service names

--- scripts/docs.ts
+++ scripts/docs.ts
@@ -3,14 +3,19 @@ import path from 'path'
 import { runGenerator } from 'ts-docgen'
 
 // Update docs/general/welcome.md to track README.md
-const README = fs.readFileSync(path.join(__dirname, '../README.md'), { encoding: 'utf8' })
+const README = fs
+  .readFileSync(path.join(__dirname, '../README.md'), { encoding: 'utf8' })
   .replace(/https:\/\/dbots\.js\.org\/#([\w/]+)/, '#$1')
 fs.writeFileSync(path.join(__dirname, '../docs/general/welcome.md'), README)
 
 // Update docs/general/changelog.md to track CHANGELOG.md
-const CHANGELOG = fs.readFileSync(path.join(__dirname, '../CHANGELOG.md'), { encoding: 'utf8' })
+const CHANGELOG = fs
+  .readFileSync(path.join(__dirname, '../CHANGELOG.md'), { encoding: 'utf8' })
   .replace(/https:\/\/dbots\.js\.org\/#([\w/]+)/, '#$1')
-fs.writeFileSync(path.join(__dirname, '../docs/general/changelog.md'), CHANGELOG)
+fs.writeFileSync(
+  path.join(__dirname, '../docs/general/changelog.md'),
+  CHANGELOG
+)
 
 // Update docs/general/services.md to reflect source changes
 var interfaceFolder = path.join(__dirname, '../src/Interface/Lists')
@@ -18,7 +23,12 @@ const listClasses = fs.readdirSync(interfaceFolder)
 var services = `# Supported Services (${listClasses.length})`
 for (const filename of listClasses) {
   const filePath = path.join(interfaceFolder, filename)
-  const { aliases, logoURL, name, websiteURL } = require(filePath).default
+  const {
+    aliases,
+    logoURL,
+    serviceName,
+    websiteURL
+  } = require(filePath).default
   const className = path.basename(filePath, '.ts')
   var ref
   if (process.env.GITHUB_REF) {
@@ -32,10 +42,12 @@ for (const filename of listClasses) {
     <img src="${logoURL}" alt="${aliases[0]} logo" width="100" align="left" />
   </p>
   ${aliases.map((key: string) => `<i id="${key}"></i>`).join('\n  ')}
-  <a href="${websiteURL}"><h1>${name}</h1></a>
+  <a href="${websiteURL}"><h1>${serviceName}</h1></a>
 
 Keys: ${aliases.map((key: string) => `\`${key}\``).join(', ')}  
-Class: [dbots.${className}](${ref ? `/#/docs/main/${ref}/class/${className}` : ''})  
+Class: [dbots.${className}](${
+    ref ? `/#/docs/main/${ref}/class/${className}` : ''
+  })  
 Website: ${websiteURL}
 </div>`
 }
