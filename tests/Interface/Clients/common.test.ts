9af4fdfce3caa4c3901a877d325b5a143370a35d
Federico Grandi <fgrandi30@gmail.com>
2021-03-26 08:37:20 +0100

    test(Clients): improve client fillers tests

--- tests/Interface/Clients/common.test.ts
+++ tests/Interface/Clients/common.test.ts
@@ -1,38 +1,114 @@
 import fs from 'fs'
 import path from 'path'
 import { ClientFiller } from '../../../src/Interface/ClientFiller'
+import Collection from '@discordjs/collection'
 
 const clientsDir = path.join(__dirname, '../../../src/Interface/Clients')
 
+const fakeClients = {
+  Discordie: {
+    Guilds: {
+      size: 123,
+      toArray: () => [1, 2, 3].map((n) => ({ member_count: n }))
+    },
+    VoiceConnections: [1, 2, 3],
+    User: { id: 'abc' },
+    options: {
+      shardId: 'abc',
+      shardCount: 123
+    }
+  },
+  DiscordIO: {
+    servers: [1, 2, 3].map((n) => ({ member_count: n })),
+    _vChannels: [1, 2, 3],
+    id: 'abc',
+    _shard: ['abc', 123]
+  },
+  DiscordJS: {
+    guilds: {
+      constructor: { name: 'GuildManager' },
+      cache: new Collection([1, 2, 3].map((n) => [n, { memberCount: n }]))
+    },
+    voice: { broadcasts: [1, 2, 3] },
+    user: { id: 'abc' },
+    shard: {
+      id: 'abc',
+      count: 123
+    }
+  },
+  Eris: {
+    guilds: new Collection([1, 2, 3].map((n) => [n, { memberCount: n }])),
+    voiceConnections: {
+      constructor: { name: 'VoiceConnectionManager' },
+      size: 123
+    },
+    user: { id: 'abc' }
+  },
+  Paracord: {
+    guilds: {
+      values: [1, 2, 3].map((n) => [n, { member_count: n }]),
+      size: 123
+    },
+    user: { id: 'abc' }
+  }
+}
+const noVoiceFillers: (keyof typeof fakeClients)[] = ['Paracord']
+const noShardFillers: (keyof typeof fakeClients)[] = ['Eris', 'Paracord']
+
 fs.readdirSync(clientsDir).forEach((file) => {
-  const fileName = path.parse(file).name
+  const fileName = path.parse(file).name as keyof typeof fakeClients
 
   describe(`${fileName} module`, () => {
     const Client = require(path.join(clientsDir, file))
       .default as typeof ClientFiller
 
     describe(`${fileName} class`, () => {
-      const c = new Client({})
+      const emptyClient = new Client({})
+      const fakeClient = fakeClients[fileName]
 
-      describe('count getters', () => {
-        test('userCount should always be a number', () => {
-          expect(['undefined', 'number']).toContain(typeof c.userCount)
-        })
-        test('serverCount should always be a number', () => {
-          expect(['undefined', 'number']).toContain(typeof c.serverCount)
-        })
-        test('voiceConnections should always be a number', () => {
-          expect(['undefined', 'number']).toContain(typeof c.voiceConnections)
-        })
-      })
+      const testClient = (c: ClientFiller, name: string, empty = false) => {
+        const eType = empty ? 'undefined' : undefined
 
-      describe('clientID getter', () => {
-        expect(['undefined', 'string']).toContain(typeof c.clientID)
-      })
+        describe(name, () => {
+          describe('count getters', () => {
+            test(`userCount should be ${eType ?? 'a number'}`, () => {
+              expect(typeof c.userCount).toBe(eType ?? 'number')
+            })
+            test(`serverCount should be ${eType ?? 'a number'}`, () => {
+              expect(typeof c.serverCount).toBe(eType ?? 'number')
+            })
 
-      describe('shard getter', () => {
-        expect(['undefined', 'object']).toContain(typeof c.shard)
-      })
+            if (!noVoiceFillers.includes(fileName))
+              test(`voiceConnections should be ${eType ?? 'a number'}`, () => {
+                expect(typeof c.voiceConnections).toBe(eType ?? 'number')
+              })
+            else
+              test(`voiceConnections should always be undefined`, () => {
+                expect(typeof c.voiceConnections).toBe('undefined')
+              })
+          })
+
+          describe('clientID getter', () => {
+            it(`should be ${eType ?? 'a string'}`, () => {
+              expect(typeof c.clientID).toBe(eType ?? 'string')
+            })
+          })
+
+          describe('shard getter', () => {
+            if (!noShardFillers.includes(fileName))
+              it('should be an object', () => {
+                expect(typeof c.shard).toBe(eType ?? 'object')
+              })
+            else
+              it('should always be undefined', () => {
+                expect(typeof c.shard).toBe('undefined')
+              })
+          })
+        })
+      }
+
+      testClient(emptyClient, 'empty client', true)
+      if (fakeClient) testClient(new Client(fakeClient), 'fake client')
     })
   })
 })
