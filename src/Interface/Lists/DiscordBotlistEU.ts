cf2eec1f1855b9db73f0f0322454e5d92b7f1560
LPTP1Dev <51876648+LPTP1Dev@users.noreply.github.com>
2021-09-17 19:16:45 +0200

    Added support for discord-botlist.eu (#318)
    * Create DiscordBotlistEU.ts
    
    * Removed unneeded argument on getVotes()
    
    * Removed unused import
    
    forgot that, sorry
    
    * ListIndex

---
+++ src/Interface/Lists/DiscordBotlistEU.ts
@@ -0,0 +1,61 @@
+import { Service, ServicePostOptions } from '../Service'
+import { Util } from '../../Utils/Util'
+
+/**
+ * Represents the DiscordBotlistEU service.
+ * @see https://docs.discord-botlist.eu/
+ */
+export default class DiscordBotlistEU extends Service {
+  /** The values that can be used to select the service. */
+  static get aliases() {
+    return ['dbleu', 'discordbotlisteu']
+  }
+
+  /** The logo URL. */
+  static get logoURL() {
+    return 'https://cdn.discord-botlist.eu/pictures/logo.png'
+  }
+
+  /** Service's name. */
+  static get serviceName() {
+    return 'DiscordBotlist.EU'
+  }
+
+  /** The website URL. */
+  static get websiteURL() {
+    return 'https://discord-botlist.eu/'
+  }
+
+  /** The base URL of the service's API. */
+  static get baseURL() {
+    return 'https://api.discord-botlist.eu/v1'
+  }
+
+  /**
+   * Posts statistics to this service.
+   * <warn>Shard data posting is not supported for this service.</warn>
+   * @param options The options of the request
+   */
+  static post(options: ServicePostOptions) {
+    const { token, serverCount } = options
+    return super._post({
+      method: 'post',
+      url: `/update`,
+      headers: { Authorization: token },
+      data: { serverCount: Util.resolveCount(serverCount) }
+    })
+  }
+
+  /**
+   * Get's the bots votes
+   */
+  getVotes() {
+    return this._request(
+      {
+        url: `/votes`,
+        headers: { Authorization: this.token }
+      },
+      { requiresToken: true }
+    )
+  }
+}
