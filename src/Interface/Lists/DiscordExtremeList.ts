35e4c34e31341257efe35b51455f322b51e3de1b
Snazzah <7025343+Snazzah@users.noreply.github.com>
2021-10-31 13:33:28 -0500

    chore(docs): update DiscordExtremeList documentation URL

--- src/Interface/Lists/DiscordExtremeList.ts
+++ src/Interface/Lists/DiscordExtremeList.ts
@@ -3,7 +3,7 @@ import { Util, IDResolvable } from '../../Utils/Util'
 
 /**
  * Represents the Discord Extreme List service.
- * @see https://docs.discordextremelist.xyz/
+ * @see https://discordextremelist.xyz/docs
  */
 export default class DiscordExtremeList extends Service {
   /** The values that can be used to select the service. */
